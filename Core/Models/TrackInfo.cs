98dd1012932ae8c51fe055d61a4c6df76ba45787
ncore <ilya.pvl@icloud.com>
2026-03-21 22:37:02 +0300

    Add date/time of track loading

--- Core/Models/TrackInfo.cs
+++ Core/Models/TrackInfo.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace AudioStemPlayer.Core.Models;
 
 public class TrackInfo
@@ -6,7 +8,11 @@ public class TrackInfo
     public string Artist { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string Album { get; set; } = string.Empty;
+    public DateTime DateAdded { get; set; } = DateTime.Now;
 
     public string DisplayName =>
         string.IsNullOrEmpty(Artist) ? Title : $"{Artist} - {Title}";
+
+    public override bool Equals(object? obj) => obj is TrackInfo other && FilePath == other.FilePath;
+    public override int GetHashCode() => FilePath?.GetHashCode() ?? 0;
 }
